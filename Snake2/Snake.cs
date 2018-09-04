f71e9d3ea6f2de03688559126e10e7b5af533c33
ghenek <gr1@mail.ru>
2018-09-04 16:18:46 +0300

    Управление змейкой

---
+++ Snake2/Snake.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    class Snake : Figure
+    {
+        public Direction direction;
+
+        public Snake(Point tail, int lenght, Direction _direction)
+        {
+            pList = new List<Point>();
+            direction = _direction;
+            for (int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.Left;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.Right;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.Down;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.Up;
+        }
+    }
+}
---
+++ Snake2/VerticalLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                pList.Add(new Point(x, y, sym));
+            }
+        }
+    }
+}
